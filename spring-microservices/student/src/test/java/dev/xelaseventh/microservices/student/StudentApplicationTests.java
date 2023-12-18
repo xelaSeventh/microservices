2f4d0c050d7c3d307fc56dd2e89e34a254814633
xelaSeventh <afx.xela@gmail.com>
2023-12-18 15:56:38 -0600

    Student Microservice Configuration

---
+++ spring-microservices/student/src/test/java/dev/xelaseventh/microservices/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xelaseventh.microservices.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
